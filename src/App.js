7c385055d0e51fe95bc1a27ace38fe2f36be628b
felixingla <felixinglavf@gmail.com>
2024-08-26 21:47:42 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Felix Ingla</h1>
+      </header>
     </div>
   );
 }
