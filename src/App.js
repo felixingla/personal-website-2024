d4d1fb68f6a85ae753b942caa1c913b69156b685
felixingla <felixinglavf@gmail.com>
2024-08-26 23:15:15 +0200

    Pedra website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
-import logo from "./logo.svg";
 import "./App.css";
+import EmailIcon from "./icons/EmailIcon";
+import LinkedinIcon from "./icons/LinkedInIcon";
+import PedraIcon from "./icons/PedraIcon";
+import FelixIngla from "./images/felix-ingla-picture.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Felix Ingla</h1>
-      </header>
+      <div className="App-content">
+        <header>
+          <h1>Felix Ingla</h1>
+          <div className="header">
+            <img
+              src={FelixIngla}
+              alt="Felix Ingla"
+              height={"80px"}
+              style={{ borderRadius: "50px" }}
+            ></img>
+            <p>
+              I am the founder of{" "}
+              <a
+                href="https://pedra.so"
+                rel="noopener"
+                alt="Felix Ingla, founder of Pedra"
+              >
+                <PedraIcon color={"#F8792A"} />
+                Pedra
+              </a>
+              , a software web application to help real estate professionals
+              create{" "}
+              <a
+                href="https://pedra.so/home-staging-virtual"
+                rel="noopener"
+                title="Virtual Home Staging with Pedra"
+              >
+                virtual home stagings
+              </a>{" "}
+              and improve their{" "}
+              <a
+                href="https://pedra.so/real-estate-photography"
+                rel="noopener"
+                title="Real Estate Photography Enhancement with Pedra"
+              >
+                real estate photography
+              </a>
+              .
+            </p>
+          </div>
+        </header>
+        <article>
+          <p>
+            Please reach out if you are passionate about well-designed real
+            estate software.
+          </p>
+          <p>
+            You can connect with me via{" "}
+            <a href="mailto:felix@pedra.so">
+              {" "}
+              <EmailIcon /> felix@pedra.so
+            </a>
+            , or via{" "}
+            <a href="https://www.linkedin.com/in/felixingla/">
+              <LinkedinIcon />
+              LinkedIn
+            </a>
+            .
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
---
+++ src/icons/EmailIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EmailIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M16.5 4.58325V7.29158L10 10.5416L3.5 7.29158V4.58325H16.5ZM3.5 8.50221V15.4166H16.5V8.50221L10 11.7522L3.5 8.50221Z"
+      fill="#878787"
+    />
+  </svg>
+);
+
+export default EmailIcon;
